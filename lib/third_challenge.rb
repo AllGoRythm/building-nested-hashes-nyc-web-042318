056c6e2d7878bbe86c15a79fef3789edc8b86f7b
Kurt Bauer <kurtbauer55@protonmail.com>
2018-04-18 20:33:31 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -14,8 +14,7 @@ def third_challenge
         :age => "15",
         :status => "alive"
         },
-      :hero_friends => [{:name => "Benvolio", :age => "17", :attitude => "worried"},
-                        {:name => "Mercutio", :age => "18", :attitude => "hot-headed"}]
+      :hero_friends => []
    },
    :capulet => {
       :patriarch => {
@@ -31,8 +30,7 @@ def third_challenge
         :age => "15",
         :status => "alive"
         },
-      :heroine_friends => [{:name => "Steven", :age => "30", :attitude => "confused"},
-                           {:name => "nurse", :age => "45", :attitude => "worried"}]
+      :heroine_friends => []
    }
   }
 
