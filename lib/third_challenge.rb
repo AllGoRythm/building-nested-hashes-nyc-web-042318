cf1daca4086b0334374bd5729ebd4177051397dd
Kurt Bauer <kurtbauer55@protonmail.com>
2018-04-18 17:02:51 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -15,21 +15,24 @@ def third_challenge
         :status => "alive"
         },
       :hero_friends => []
-   }, 
+   },
    :capulet => {
       :patriarch => {
         :name => "Lord Capulet",
         :age => "50"
         },
       :matriarch => {
-        
+        :name => "Lady Capulet",
+        :age => "15"
         },
       :heroine => {
-        #your key/value pairs here
+        :name => "Juliet",
+        :age => "15",
+        :status => "alive"
         },
       :heroine_friends => []
    }
   }
 
-  
+
 end
